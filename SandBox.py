7abb20058358710d8af2f252418b1f71ec3a4129
BenGarlock <ben.garlock@gmail.com>
2020-10-13 08:40:39 -0400

    first commit

---
+++ SandBox.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+zip_code = "10128"
+country_code = "us"
+api_url = f"http://api.openweathermap.org/data/2.5/weather?zip={zip_code},{country_code}&appid={api_key}"
+
+
+def get_weather():
+    data = requests.request("GET", api_url).content
+    output = json.loads(data)
+    weather = output['weather'][0]['main']
+    city = output['name']
+    if weather == 'Mist':
+        weather = "Misty"
+    return (f'Today the weather is {weather} in {city}')
+
+
+print(get_weather())
